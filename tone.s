c13523c7d5730b702f59fb296f24e4944bad64d7
Micah Cowan <micah@addictivecode.org>
2021-11-01 02:23:00 -0700

    Include the "continue" test

--- tone.s
+++ tone.s
@@ -1,11 +1,21 @@
         PITCH  = $7
         SPKR   = $C030
         .org $300
-        LDY #0
+START:  LDY #0
 FREQ:   DEX
-        BNE FREQ
+CONT:   BNE FREQ
 TWEAK:  LDA SPKR
         LDX PITCH
         DEY
         BNE FREQ
 DONE:   RTS
+
+        BRK
+        LDA #$FC
+        STA STO+1
+        LDA #$FF
+        STA PITCH
+        JSR START
+        LDA #$FF
+STO:    LDX #$F2
+        JSR CONT
