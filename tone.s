58fbd4fdd40d526c0dc0c44974bb208112f330f7
Micah Cowan <micah@addictivecode.org>
2021-10-31 22:31:06 -0700

    Tweaks to make continued tones possible

--- tone.s
+++ tone.s
@@ -1,14 +1,11 @@
-        COUNTR = $6
         PITCH  = $7
         SPKR   = $C030
         .org $300
+        LDY #0
+FREQ:   DEX
+        BNE FREQ
 TWEAK:  LDA SPKR
         LDX PITCH
-COUNT:  DEY
+        DEY
         BNE FREQ
-        DEC COUNTR
-        BEQ DONE
-FREQ:   DEX
-        BNE COUNT
-        BEQ TWEAK
 DONE:   RTS
