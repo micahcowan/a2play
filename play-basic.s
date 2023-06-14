ff6beec5ace163a2ad5f590514d20a5c6504182a
Micah Cowan <micah@addictivecode.org>
2023-06-14 11:25:44 -0700

    Test "bad spec" on note name

--- play-basic.s
+++ play-basic.s
@@ -5,8 +5,11 @@
 ASoftProg:
 	lineP "PLAYING MUSIC..."
         line "A$ = ",'"',"A ",'"'
+        line "Q$ = ",'"',"Q ",'"'
         line "& PLAYE, A$+",'"',"C",'"',": ?",'"',"REST",'"'
-        line "& PLAY, A$+",'"',"C",'"',": ?",'"',"REST",'"'
+FIX_LINE .set LINE_NUMBER
+	line "& PLAY, A$+Q$+",'"',"C",'"',": ?",'"',"REST",'"'
         lineP "AFTER"
         scrcode "RUN", $0D
+        scrcode "Q$=",'"','"',":GOTO ",.sprintf("%d",FIX_LINE),$0D
 ASoftEnd:
