ba3c8835043dae6e202224e79b962c15109e2e42
Micah Cowan <micah@addictivecode.org>
2023-06-14 23:07:52 -0700

    Note names to base pitches

--- play.s
+++ play.s
@@ -27,18 +27,43 @@ NextAmper:
 PlayTag:
 	.byte "PLAY,"
         .byte $00
+BasePitch:
+	; AppleSoft floating-point representation
+        ; of the number 37190, the length of
+        ; one-half of the waveform of the A note
+        ; that is an octave below the lowest A note
+        ; supported by this program, as measured
+        ; in 1.023MHz cycles.
+	.byte $90, $91, $46, $00, $00
+HalfStep:
+	; AppleSoft floating-point representation
+        ; of 2^(1/12), or 1.0594..., the ratio of
+        ; the frequency of one musical note, to
+        ; the note a half-step below it.
+        ; Divide a half-wavelength by this number
+        ; to get the half-wavelength a half-step
+        ; above it.
+	.byte $81, $87, $9C, $7C, $96
 MsgBad:
 	scrcode $0D,"?BAD NOTE SPEC: "
         .byte 0
 StrRemain:
 	.byte $00
+StrSpec = $6
+StrSaved:
+	.word 0
 AmperPlay:
 	jsr CheckTag
         bcs YesItsUs
 	jmp NextAmper
 	rts
 YesItsUs:
-	jsr PrepNoteStr
+	; Save away ZP items we may overwrite with StrSpec
+	lda $6
+        pha
+        lda $7
+        pha
+        jsr PrepNoteStr
 .if 0
         lda AS_INDEX+1
         jsr Mon_PRBYTE
@@ -58,6 +83,10 @@ YesItsUs:
 @out:
         lda #$8D
         jsr Mon_COUT
+        pla
+        sta $7
+        pla
+        sta $6
 	rts
 .endif
 @loop:
@@ -67,6 +96,14 @@ YesItsUs:
         bne @loop
 
 DoNextNote:
+	; Save the current position as the start
+        ;  of the note spec, so we can print
+        ;  it out later if there's a problem with it.
+	lda StrSpec
+        sta StrSaved
+        lda StrSpec+1
+        sta StrSaved+1
+        
 	jsr StrGetCur
         beq @exit
 	jsr GetNoteBasePitch
@@ -82,6 +119,19 @@ DoNextNote:
 @exit:
 	rts
 
+PrintFacBytes:
+	ldx #0
+:
+	lda AS_FAC, x
+        jsr Mon_PRBYTE
+        lda #$A0
+        jsr Mon_COUT
+        inx
+        cpx #5
+        bne :-
+        jsr Mon_CROUT
+	rts
+
 InitPitch:
 	
 	rts
@@ -94,8 +144,38 @@ GetNoteBasePitch:
         bcc NoteNameErr ; < 'A', bail
         tax
         lda NoteSteps,x
-        ; XXX Adjust base pitch
-@found:
+        
+        ; We have N, we want HalfStep^N.
+        
+        ; First, convert our integer exponent
+        ;  into floating-point
+        tay
+        lda #0
+        jsr AS_GIVAYF
+        ; Get pointer to HalfStep into A, Y
+        ; ...and do the exponentiation (^)
+        lda #<HalfStep
+        ldy #>HalfStep
+        jsr AS_LOAD_ARG_FROM_YA
+        jsr AS_FPWRT
+        
+        ; Now we need to divide BasePitch by our
+        ;  "steps" result, to get the base pitch
+        ;  for *this* note.
+        lda #<BasePitch
+        ldy #>BasePitch
+        jsr AS_FDIV
+        ldx #0
+        stx $A2 ; ensure a positive result
+        .if 0
+        jsr AS_AYINT
+        ldy $A1
+        lda $A0
+        jsr AS_GIVAYF
+        .endif
+        jsr PrintFacBytes
+        jsr Mon_CROUT
+        jsr AS_PRINT_FAC
 	rts
 NoteNameErr:
 	; handle note spec error
@@ -145,19 +225,23 @@ PrepNoteStr:
 	jsr AS_FRMEVL
         jsr AS_FREFAC
         sta StrRemain
+        lda AS_INDEX
+        sta StrSpec
+        lda AS_INDEX+1
+        sta StrSpec+1
         rts
 StrGetNext:
 	lda StrRemain
         beq StrRts
         dec StrRemain
-        inc AS_INDEX
+        inc StrSpec
         bne StrGetCur
-        inc AS_INDEX+1
+        inc StrSpec+1
 StrGetCur:
 	lda StrRemain
         beq @done
         ldy #0
-        lda (AS_INDEX),y
+        lda (StrSpec),y
         ; convert to uppercase, ASCII
         cmp #$61
         bcc @skipCvt
