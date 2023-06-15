9fda080845fe1f59c05bf909c09410fbd8477a5f
Micah Cowan <micah@addictivecode.org>
2023-06-15 00:07:46 -0700

    Identify the octave #

--- tones.c
+++ tones.c
@@ -5,14 +5,16 @@ int
 main(void)
 {
     double halfStep = pow(2., 1./12.);
-    double startTone = 27.5;    // Freq of tone A0
+    double startTone = 27.5/2.; // Freq of tone A0
     double cpu = 1022727.0;     // Freq of Apple 2's 6502 CPU
+    double octStart;
+    unsigned int oct=0;
 
     printf("%f\n\n", halfStep);
 
     for (double octStart = startTone; octStart < 4200.0; octStart *= 2) {
         double tone = octStart;
-        printf("[%.2f]\n", octStart);
+        printf("[%u: %.2f]\n", oct, octStart);
         for (int i=0; i<13; ++i) {
             printf("(%.2f) %lu : ", tone, (unsigned long)(cpu/tone/2.));
             tone *= halfStep;
@@ -23,6 +25,7 @@ main(void)
             }
         }
         putchar('\n');
+        ++oct;
     }
     return 0;
 }
